ca8e47734f3f557f1dbb3294c6ee68423c499b6b
Akisha0707 <139120038+Akisha0707@users.noreply.github.com>
2023-11-27 19:12:03 +0000

    Deploying to gh-pages from @ Akisha0707/goit-js-homework-09@4d888a216483d7c16f6422870331b787223af8a6 🚀

---
+++ 03-promises.8e60b519.js
@@ -0,0 +1,2 @@
+document.querySelector(".form").addEventListener("click",(function(e,o){Math.random()>.3?console.log(`✅ Fulfilled promise ${e} in ${o}ms`):console.log(`❌ Rejected promise ${e} in ${o}ms`)}));
+//# sourceMappingURL=03-promises.8e60b519.js.map

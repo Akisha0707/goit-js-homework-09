ffc8ebdbb21f01c47c6ba48cbdab0992f570a95c
Akisha0707 <139120038+Akisha0707@users.noreply.github.com>
2023-11-27 10:56:36 +0000

    Deploying to gh-pages from @ Akisha0707/goit-js-homework-09@378469579aadf2be7953e1f52be969a6946c1d0e 🚀

---
+++ 01-color-switcher.2cebd7e4.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("body"),o=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");o.addEventListener("click",(function(n){t=setInterval((function(){console.log(e.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))),console.log("gello")}),1e3),o.disabled=!0})),n.addEventListener("click",(function(e){clearInterval(t),o.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.2cebd7e4.js.map

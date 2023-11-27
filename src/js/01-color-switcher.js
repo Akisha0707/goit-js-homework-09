378469579aadf2be7953e1f52be969a6946c1d0e
Akisha0707 <139120038+Akisha0707@users.noreply.github.com>
2023-11-27 12:56:07 +0200

    change files js 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
-console.log('hello');
+const bodyElement = document.querySelector('body');
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+let getColor;
+
+buttonStart.addEventListener('click', startColor);
+buttonStop.addEventListener('click', stopColor);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function startColor(event) {
+  getColor = setInterval(() => {
+    console.log((bodyElement.style.backgroundColor = getRandomHexColor()));
+    console.log('gello');
+  }, 1000);
+  buttonStart.disabled = true;
+}
+
+function stopColor(evt) {
+  clearInterval(getColor);
+  buttonStart.disabled = false;
+}

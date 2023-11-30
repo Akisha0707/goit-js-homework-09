abdcc3052f8670941869035df34eec0d30f30329
Akisha0707 <139120038+Akisha0707@users.noreply.github.com>
2023-11-30 09:58:01 +0000

    Deploying to gh-pages from @ Akisha0707/goit-js-homework-09@29497b7ef6599b447ca1ae22598048348aac9296 🚀

--- 03-promises.8e60b519.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form").addEventListener("click",(function(e,o){Math.random()>.3?console.log(`✅ Fulfilled promise ${e} in ${o}ms`):console.log(`❌ Rejected promise ${e} in ${o}ms`)}));
-//# sourceMappingURL=03-promises.8e60b519.js.map
--- 03-promises.f1b3fef3.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form").addEventListener("click",(function(o,c){Math.random()>.3?console.log("✅ Fulfilled promise ".concat(o," in ").concat(c,"ms")):console.log("❌ Rejected promise ".concat(o," in ").concat(c,"ms"))}));
-//# sourceMappingURL=03-promises.f1b3fef3.js.map
